ee280eb21014cdcc603a35e392349b1e929ccd1f
didgmlcks99 <heechy06@naver.com>
2021-03-18 19:06:35 +0900

    3.18 push

---
+++ sorting.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+void print(int a[], int n){
+  for(int i=0; i<n; i++){
+    printf("%d, ", a[i]);
+  }printf("\n");
+}
+
+void swap(int *xp, int *yp) { 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+  
+void selectionSort(int arr[], int n) { 
+    int i, j, min_idx; 
+  
+    // One by one move boundary of unsorted subarray 
+    for (i = 0; i < n-1; i++) 
+    { 
+        // Find the minimum element in unsorted array 
+        min_idx = i; 
+        for (j = i+1; j < n; j++) 
+          if (arr[j] < arr[min_idx]) 
+            min_idx = j; 
+        // Swap the found minimum element with the first element 
+        swap(&arr[min_idx], &arr[i]); 
+    } 
+} 
+
+void bubbleSort(int arr[], int n) { 
+   int i, j; 
+   for (i = 0; i < n-1; i++)       
+       // Last i elements are already in place    
+       for (j = 0; j < n-i-1; j++)  
+           if (arr[j] > arr[j+1]) 
+              swap(&arr[j], &arr[j+1]); 
+} 
+
+void recbubbleSort(int arr[], int n){
+    // Base case
+    if (n == 1)
+        return;
+ 
+    // One pass of bubble sort. After
+    // this pass, the largest element
+    // is moved (or bubbled) to end.
+    for (int i=0; i<n-1; i++)
+        if (arr[i] > arr[i+1])
+            swap(&arr[i], &arr[i+1]);
+ 
+    // Largest element is fixed,
+    // recur for remaining array
+    recbubbleSort(arr, n-1);
+}
+
+void insertionSort(int arr[], int n){
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+        
+        /* Move elements of arr[0..i-1], that are
+          greater than key, to one position ahead
+          of their current position */
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+ 
+
+int main(){
+  int a[10] = {20, 5, 30, 60, 40, 58, 28, 16, 43, 21};
+  int n = sizeof(a)/sizeof(int);
+
+  print(a, n);
+
+  // bubbleSort(a, n);
+  // selectionSort(a, n);
+  // recbubbleSort(a, n);
+  insertionSort(a, n);
+
+  print(a, n);
+
+  return 0;
+}
